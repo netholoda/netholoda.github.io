4b6d1fecbccd065172db99306d0bacee97ddb507
Boris Hasikov <hasikov@laapl.ru>
2023-07-14 17:35:50 +0300

    Встроенные картинки

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  build: { assetsInlineLimit: 1024 * 500 },
   base: "/promo"
 })
